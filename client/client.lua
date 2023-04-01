5dd2b99c071de8be806ad3aa8c0f4b68ae760ca6
SYNO® <89760730+SYNO-SY@users.noreply.github.com>
2023-04-01 16:56:48 +0400

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -273,7 +273,7 @@ function GetClosestPlayer(radius)
 	local closestPlayer = -1
 	local ply = PlayerPedId()
 	local plyCoords = GetEntityCoords(ply, 0)
-	for index,value inpairs(players) do
+	for index,value in pairs(players) do
 		local target = GetPlayerPed(value)
 		if(target ~= ply) then
 			local targetCoords = GetEntityCoords(GetPlayerPed(value), 0)
